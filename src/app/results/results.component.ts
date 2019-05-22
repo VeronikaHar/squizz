414063323baeeed55801cd64ceeb919f383fbf84
veronikahar <veronika.har@hotmail.com>
2019-05-22 17:32:26 -0500

    Component to display quiz results created.

---
+++ src/app/results/results.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Answers } from '../quiz.model';
+
+@Component({
+  selector: 'app-results',
+  templateUrl: './results.component.html',
+  styleUrls: ['./results.component.scss']
+})
+export class ResultsComponent {
+  @Input() answers: Answers;
+}
